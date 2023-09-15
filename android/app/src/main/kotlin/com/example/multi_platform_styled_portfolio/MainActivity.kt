0987f032b210cd4ff5286f6b50be54b73cc88fb1
Aneeb Baig <aneebbaig123@gmail.com>
2023-09-15 23:14:31 +0500

    Project init

---
+++ android/app/src/main/kotlin/com/example/multi_platform_styled_portfolio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.multi_platform_styled_portfolio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/view/widgets/appbar.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  final String title;
+  const CustomAppBar({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(
+        title,
+      ),
+      leading: const Icon(
+        Icons.apple,
+      ),
+      actions: const [],
+    );
+  }
+}
