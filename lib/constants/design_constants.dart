d0628132a19163c7ce8b8be86265c3d1757e23c0
Aneeb Baig <aneebbaig123@gmail.com>
2023-09-16 23:25:36 +0500

    Defining colors

---
+++ lib/constants/design_constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class DesignConstants {
+  // Windows 11
+  static const winSeedColor = Color(0xFF007ACC);
+
+  //MacOS Mohave
+  static const Color macPrimaryColor = Color(0xFF282828);
+  static const Color macSecondaryColor = Color(0xFF303030);
+  static const Color macAccentColor = Color(0xFF4CAF50);
+
+  //Iphone 14
+
+  static const Color iphoneSeedColor = Color(0xFF282828);
+
+  //Pixel 7
+
+  static const Color pixelSeedColor = Color(0xFF4285F4);
+}
